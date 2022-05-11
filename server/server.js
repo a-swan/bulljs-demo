ef7a971ddbc7cabc5d6fc6eb19ac505ad798f38d
alexpswanson <alexpswanson@gmail.com>
2022-05-11 12:50:21 +0300

    added status info to job.data

--- server/server.js
+++ server/server.js
@@ -5,21 +5,26 @@ try{
   logQueue.process("logQueue", async (job) => {
     console.log(JSON.stringify(job.data.body));
 
-    if(job.opts.timestamp % 2 === 0 ){
+    if(job.processedOn % 2 === 0 ){
       //succeed
+      job.data.status = "success"
     }else{
+      job.data.status = "error"
       return Promise.reject('arbitrary failure');
     }
   });
 
   logQueue.on("completed", (job) => {  
-    job.remove();
     console.log('Completed: ' + JSON.stringify(job));
   });
 
   logQueue.on("failed", async (job) => {
     console.log('Failed: ' + JSON.stringify(job.data.body) + job.attemptsMade);
     console.log('Job: ' + JSON.stringify(job))
+
+    if(job.attemptsMade == job.opts.attempts){
+      console.log('Removing from queue. Process permanently failed job below')
+    }
   })
 
 }catch(err){
